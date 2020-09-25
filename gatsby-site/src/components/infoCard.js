93a521a56f84d1dd6c4be040573cc1c4162263fd
Harrison Quarry <harrisonqleddy@gmail.com>
2020-09-25 04:47:19 -0700

    Fixed featurette infoCards - added content

--- gatsby-site/src/components/infoCard.js
+++ gatsby-site/src/components/infoCard.js
@@ -1,51 +1,16 @@
 import React from "react"
 import { Card } from "react-bootstrap"
 
-const Topics = {
-  default: {
-    title: "Default Title",
-    text: "Text Description"
-  },
-  registerToVote: {
-    title: "Register to Vote",
-    text: "Register to Vote now"
-  },
-  absenteeVoting: {
-    title: "Absentee Voting",
-    text: "Absentee"
-  },
-  voteByMail: {
-    title: "Vote By Mail",
-    text: "Mail mail"
-  },
-  earlyVoting: {
-    title: "Early Voting",
-    text: "Voting Early"
-  },
-  idLaws: {
-    title: "Id Laws",
-    text: "Learn about ID laws"
-  }
-}
-
-const InfoCard = ({topic, className, children}) => {
-  if( Topics[topic] == undefined ){
-    topic = 'default'
-  }
-
-  return (
-    <Card className={className}>
-      <svg className="card-img-top" style={{height:"30px"}}>
-        <rect width="100%" height="100%" style={{fill:"crimson"}}/>
-        <polygon points="15.15, 1.52 6.06, 30.00 28.79, 11.82 1.52, 11.82 24.24, 30.00" style={{fill:"white"}}/>
-      </svg>
-      <Card.Body>
-        <h4>{Topics[topic].title}</h4>
-        <p>{Topics[topic].text}</p>
-        {children}
-      </Card.Body>
-    </Card>
-  )
-}
+const InfoCard = ({className, children}) => (
+  <Card className={className}>
+    <svg className="card-img-top" style={{height:"30px"}}>
+      <rect width="100%" height="100%" style={{fill:"crimson"}}/>
+      <polygon points="15.15, 1.52 6.06, 30.00 28.79, 11.82 1.52, 11.82 24.24, 30.00" style={{fill:"white"}}/>
+    </svg>
+    <Card.Body>
+      {children}
+    </Card.Body>
+  </Card>
+)
 
 export default InfoCard
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -26,43 +26,74 @@ const IndexPage = () => {
 
 
   return (
-  <Layout>
-    <SEO title="Vote" description="Vote today!"/>
-    <Banner image="americanFlag">
-      <Row>
-        <div className="col-md-6 container bg-black" style={{margin:'auto 0'}}>
-          <h1 className="hero-text">Vote!</h1>
-          <p id="sub-hero-text">With...</p>
-          <p id="hero-phrase">{tagline}</p>
-        </div>
-        <div className="col-md-6">
-          <p className="header-quote">
-            The only thing necessary for the triumph of <span style={{color:"red"}}>evil</span> is for good men and women to do <span style={{color:"yellow"}}>nothing</span>
-          </p>
-        </div>
-      </Row>
-    </Banner>
+    <Layout>
+      <SEO title="Vote" description="Vote today!"/>
+      <Banner image="americanFlag">
+        <Row>
+          <div className="col-md-6 container bg-black" style={{margin:'auto 0'}}>
+            <h1 className="hero-text">Vote!</h1>
+            <p id="sub-hero-text">With...</p>
+            <p id="hero-phrase">{tagline}</p>
+          </div>
+          <div className="col-md-6">
+            <p className="header-quote">
+              The only thing necessary for the triumph of <span style={{color:"red"}}>evil</span> is for good men and women to do <span style={{color:"yellow"}}>nothing</span>
+            </p>
+          </div>
+        </Row>
+      </Banner>
 
-    <Container>
-      <Row>
-        <Col md={5} style={{paddingRight:"10px"}}>
-          <GeneralElectionInfo/>
-        </Col>
-        <Col xs={{ order:"first" }} md style={{paddingLeft:"10px"}}>
-          <StateElectionDates/>
-        </Col>
-      </Row>
-    </Container>
+      <Container>
+        <Row>
+          <Col md >
+            <StateElectionDates/>
+          </Col>
+          <Col id="genElectionInfo" md={5}>
+            <GeneralElectionInfo/>
+          </Col>
+        </Row>
+      </Container>
 
-    <Container style={{marginBottom:"2rem"}}>
-      <InfoCard className="featurette" topic="registerToVote"/>
-      <InfoCard className="featurette" topic="absenteeVoting"/>
-      <InfoCard className="featurette" topic="voteByMail"/>
-      <InfoCard className="featurette" topic="earlyVoting"/>
-      <InfoCard className="featurette" topic="idLaws"/>
-    </Container>
-
-  </Layout>
+      <Container style={{marginBottom:"2rem"}}>
+        <InfoCard className="featurette">
+          <Row>
+            <Col md="4" lg="3"><h4>Register to Vote</h4></Col>
+            <Col>
+              <p>Each state makes its own voting and election rules, including when and how to register. Check with your state or local election office to get the most detailed and up-to-date information for where you live.</p>
+              <p>Every state except North Dakota requires citizens to register if they want to become voters. Depending on your state, the registration deadline could be as much as a month before an election.</p>
+            </Col>
+          </Row>
+        </InfoCard>
+        <InfoCard className="featurette">
+          <Row>
+            <Col md="4" lg="3"><h4>Absentee Voting</h4><h4>+ Vote By Mail</h4></Col>
+            <Col>
+              <p>Absentee voting (aka “mail-in voting” and “by-mail voting”) is conducted by mail-in ballot before the day of Election Day. All states will mail a ballot to voters if certain conditions are met. The voter may return the ballot in person or by mail. Some states will let voters apply for an absentee ballot in person before Election Day and then vote the ballot that same day.</p>
+            </Col>
+          </Row>
+        </InfoCard>
+        <InfoCard className="featurette">
+          <Row>
+            <Col md="4" lg="3"><h4>Early Voting</h4></Col>
+            <Col>
+              <p>Sometimes circumstances make it hard or impossible for you to vote on Election Day. But your state may let you vote during a designated early voting period.
+                <strong> Please note: Your state's rules and voting dates may change in 2020 due to the coronavirus pandemic</strong></p>
+              <ul>
+                <li>Most states have early voting. This lets registered voters vote on specified dates before Election Day.</li>
+                <li>You don't need an excuse to vote early.</li>
+                <li>In some states, you may cast an absentee ballot in person before Election Day. To do this, you must request an absentee ballot from your state. Your state may require you to submit a valid excuse too.</li> 
+              </ul>
+            </Col>
+          </Row>
+        </InfoCard>
+        <InfoCard className="featurette">
+          <Row>
+            <Col md="4" lg="3"><h4>ID laws</h4></Col>
+            <Col><p>Other stuff</p></Col>
+          </Row>
+        </InfoCard>
+      </Container>
+    </Layout>
   )
 }
 
